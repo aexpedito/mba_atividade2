b8bbcda96e101ebe9d62ecd257afa723cf6792a7
aexpedito <aexpedito@hotmail.com>
2024-03-19 20:29:36 -0300

    First commit

---
+++ notes.sql
@@ -0,0 +1,58 @@
+--issue with PS shell to execute scripts
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+--then enable venv
+.\venv\Scripts\activate.ps1
+
+
+
+
+
+
+{
+   "136a50f6-dec8-4726-a63a-7980c6e27f0b":{
+      "this":<__main__.Person object at 0x000001EBDAC1ACD0>,
+      "friends":[
+         <__main__.Person object at 0x000001EBDAFB7890>,
+         <__main__.Person object at 0x000001EBDAFB7850>,
+         <__main__.Person object at 0x000001EBDAFB7990>,
+         <__main__.Person object at 0x000001EBDAFB7950>
+      ]
+   },
+   "1d137d17-f8fc-41cb-a622-2090d1ff3bfd":{
+      "this":<__main__.Person object at 0x000001EBDAFB7890>,
+      "friends":[
+         <__main__.Person object at 0x000001EBDAC1ACD0>,
+         <__main__.Person object at 0x000001EBDAFB7850>,
+         <__main__.Person object at 0x000001EBDAFB7950>,
+         <__main__.Person object at 0x000001EBDAFB7990>
+      ]
+   },
+   "30ffabbd-d9e8-4f73-9ae3-ac6f23b4bf21":{
+      "this":<__main__.Person object at 0x000001EBDAFB7850>,
+      "friends":[
+         <__main__.Person object at 0x000001EBDAFB7890>,
+         <__main__.Person object at 0x000001EBDAFB7950>,
+         <__main__.Person object at 0x000001EBDAFB7990>,
+         <__main__.Person object at 0x000001EBDAC1ACD0>
+      ]
+   },
+   "d0a1d09b-9768-4087-9ce0-01cf9ec36a4c":{
+      "this":<__main__.Person object at 0x000001EBDAFB7950>,
+      "friends":[
+         <__main__.Person object at 0x000001EBDAFB7850>,
+         <__main__.Person object at 0x000001EBDAFB7990>,
+         <__main__.Person object at 0x000001EBDAC1ACD0>,
+         <__main__.Person object at 0x000001EBDAFB7890>
+      ]
+   },
+   "0455e2c9-abb0-4f8c-8113-0f1082be6ed0":{
+      "this":<__main__.Person object at 0x000001EBDAFB7990>,
+      "friends":[
+         <__main__.Person object at 0x000001EBDAFB7950>,
+         <__main__.Person object at 0x000001EBDAFB7850>,
+         <__main__.Person object at 0x000001EBDAC1ACD0>,
+         <__main__.Person object at 0x000001EBDAFB7890>
+      ]
+   }
+}
+
